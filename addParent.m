f3f5c3f9a7613be7c1fdbe270045f0f08111cb36
Mika Valtonen <mika.valtonen@tuni.fi>
2020-05-03 21:17:45 +0300

    Korjattu addParent

--- addParent.m
+++ addParent.m
@@ -1,17 +1,18 @@
 function A = addParent(A,root)
 
-if A(root,1)== -1 && A(root,2)== -1
-    A(root,3) = root;
-elseif A(root,1)== -1
-    A(root,3) = root;
-    A = addParent(A,A(root,2));
-elseif A(root,2)== -1
-    A(root,3) = root;
-    A = addParent(A,A(root,1));
-else
-    A(root,3) = root;
-    A = addParent(A,A(root,1));
-    A = addParent(A,A(root,2));
+if A(root,1)~= -1 && A(root,2)~= -1
+    newRoot1 = A(root,1);
+    A(newRoot1,3) = root;
+    A = addParent(A,newRoot1);
+    newRoot2 = A(root,2);
+    A(newRoot2,3) = root;
+    A = addParent(A,newRoot2);
+elseif A(root,1)~= -1 && A(root,2)== -1
+    newRoot = A(root,1);
+    A(newRoot,3) = root;
+    A = addParent(A,newRoot);
+elseif A(root,1)== -1 && A(root,2)~= -1
+    newRoot = A(root,2);
+    A(newRoot,3) = root;
+    A = addParent(A,newRoot);
 end
-
-    
--- main.m
+++ main.m
@@ -1,7 +1,5 @@
-[A,root] = randBST(5);
+[A,root] = randBST(10);
 A = addParent(A,root);
-A(root,3) = 0;
-A;
-% En saa A(root,3) nollaksi muuten kuin täältä mainin kautta
-% Jos keksit miten sen saa tehtyä tuossa addParent-funtiossa niin hieno
-% homma 
+root
+A
+plotTreeA(A)
