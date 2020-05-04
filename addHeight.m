1318557369921697b88d109a81ead8d11054c2cc
Mika Valtonen <mika.valtonen@tuni.fi>
2020-05-04 08:05:30 +0300

    Muokattu addHeight hahmotelma

--- addHeight.m
+++ addHeight.m
@@ -1,23 +1,29 @@
 function A = addHeight(A, root)
 
-% Silmukassa käydään koko taulukko A läpi
+% Käydään silmukassa taulukko A läpi ja lisätää 4 sarake eli polun pituus
 for i = 1:length(A)
-    if A(i,1)~= -1 && A(i,2)~= -1
-        % selvitetään regressiolla puun korkeus 
-        A = polunPituus(A(i),root) %vasen alipuu, funktion kutsu hiukan sinnepäin...
-        A = polunPituus(A(i),root) %oikea alipuu
-    elseif A(i,1)~= -1 && A(i,2)== -1
-        A = polunPituus(A(i),root) % vasen alipuu
-    elseif A(i,1)== -1 && A(i,2)~= -1
-        A = polunPituus(A(i),root) % oikea alipuu
+    if A(i,1) == -1 && A(i,2) == -1
+        A(i,4) = 0;
+    else
+        A(i,4) = polunPituus(A,A(i,3))
     end
 end
 
-
-    function A = polunPituus(A,i)
-        % A = palauttaa aina 1 kun pisteiden välillä on viiva
-        % eli 1+1+1+...
+% TÄSTÄ PITÄISI KEHITTÄÄ TOIMIVA FUNKTIO...
+    function pituus = polunPituus(A,i)
+        % selvitetään regressiolla puun korkeus
+        % palauttaa aina 1 JA lisätään se pituuteen 
+        % kun pisteiden välillä on viiva eli 1+1+1+...
+        
+        vasenAliPuu = polunPituus(A(i),i)
+        oikeaAliPuu = polunPituus(A(i),i)
+        % verrataan alipuiden pituutta ja lisätään pitempi pituuteen
+        pituus = pituus + max(vasenAliPuu,oikeaAliPuu)
     end
+
+end
 
 
 end
+
+      
