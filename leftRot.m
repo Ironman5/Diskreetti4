827861147934147571f9d90192e19ad45a41a106
Arska81 <64403125+Arska81@users.noreply.github.com>
2020-05-08 11:12:00 +0300

    Update leftRot.m
    päivitetyt heighit X:lle Z:lle ja A:lle

--- leftRot.m
+++ leftRot.m
@@ -21,26 +21,19 @@ else
 end
 
 % 7. compute the height of the subtree whose root is X
-Y = LR(X,1);
-T3 = LR(X,2)
-
-LR(X,4) = max(Y+1,T3+1)
-
-% 7. compute the height of the subtree whose root is X
-T3_pituus = LR(T3,4)
-Y=LR(LR(X,1),4) %Y = X:n vasen alihaaran pituus
-LR(X,4)=max(Y+1,T3_pituus+1) % X:n pituus
+T3_pituus = LR(T3,4);
+Y=LR(LR(X,1),4);              %Y = X:n vasen alihaaran pituus
+LR(X,4)=max(Y+1,T3_pituus+1); % X:n pituus
 
 % 8. compute the height of the subtree whose root is Z
-LR(Z,4)=LR(Z,4)+1 % Z:n pituus
+T4 = LR(Z,2);
+T4_pituus = LR(T4,4);
+X_pituus=LR(X,4);
+LR(Z,4)=max(X_pituus+1, T4_pituus+1); % Z:n pituus
 
 % 9. compute the height of the subtree whose root is A
-LR(root,4)=LR(root,4) % A:n pituus
-
-% 7. compute the height of the subtree whose root is X
-
-% 8. compute the height of the subtree whose root is Z
-
-% 9. compute the height of the subtree whose root is A
+A_vasenpituus = LR(LR(root,1),4);
+Z_pituus=LR(Z,4);
+LR(root,4)=max(Z_pituus+1,A_vasenpituus+1); % A:n pituus
 
 end
